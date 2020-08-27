727ba74fe29a2390140a7a28c1c56d5c99ae7a35
Heath Mast <hrmast@gmail.com>
2020-08-27 22:27:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class Song
+  include Paramable
+  extend Findable
+  extend Memorable
+
   attr_accessor :name
   attr_reader :artist
 
@@ -10,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -30,7 +34,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
