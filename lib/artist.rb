8767cb907c2e32f37f17632a6b1fc60c796f174d
Heath Mast <hrmast@gmail.com>
2020-08-27 23:18:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   include Paramable
   extend Findable
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
+  include Memorable :: InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
