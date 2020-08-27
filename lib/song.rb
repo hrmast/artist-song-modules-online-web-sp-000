84b146370c3fd57343646773fc26c5bce63c9a88
Heath Mast <hrmast@gmail.com>
2020-08-27 23:33:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,9 @@ class Song
   #   @@songs.detect{|a| a.name == name}
   # end
 
-  def self.all
-    @@songs
-  end
+  # def self.all
+  #   @@songs
+  # end
 
   # def self.reset_all
   #   self.all.clear
