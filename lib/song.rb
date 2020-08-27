e109007e08fbd62cb5de4c6bf8413602072a713c
Heath Mast <hrmast@gmail.com>
2020-08-27 22:45:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,16 +11,16 @@ class Song
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
   # end
 
-  def self.all
-    @@songs
-  end
+  # def self.all
+  #   @@songs
+  # end
 
   def self.reset_all
     self.all.clear
