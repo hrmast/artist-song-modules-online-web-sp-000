fa602c58e91f98cc5b35c3fa08950c61a75bdc07
Heath Mast <hrmast@gmail.com>
2020-08-28 00:03:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,10 +20,6 @@ class Artist
     @@artists
   end
 
-  def find_by_name(name)
-    @@artists.detect {|a| a.name == name}
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
